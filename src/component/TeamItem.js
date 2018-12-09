b83f3399e38442809e820d1759e2b4f8e8ead8aa
unknown <ousemakotti@gmail.com>
2018-12-09 14:44:10 +0100

    REACT #4 - STYLING

--- src/component/TeamItem.js
+++ src/component/TeamItem.js
@@ -1,21 +1,39 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../App.css";
 
+class TeamItem extends Component {
+  render() {
+    const styles = {
+      lastName: {
+        fontSize: 14,
+        fontWeight: "bold"
+      },
+      infos: {
+        fontSize: 6
+      }
+    };
 
-
-class TeamItem extends Component{
-    render(){
-        return(
-            <div className={this.props.container}>    
-                <img className={this.props.styleImage} src={this.props.source} alt="profile"/>
-                <br/>
-                <span className={this.props.className}>{this.props.firstName}</span>
-                <span className="lastName">{` ${this.props.lastName}`}<br/></span>
-                <span className="infos">{this.props.grade}<br/></span>
-                <span className="infos">{this.props.post}</span>
-            </div>
-        )
-    }
+    return (
+      <div className={this.props.container}>
+        <img
+          className={this.props.styleImage}
+          src={this.props.source}
+          alt="profile"
+        />
+        <br />
+        <span className={this.props.className}>{this.props.firstName}</span>
+        <span style={styles.lastName}>
+          {` ${this.props.lastName}`}
+          <br />
+        </span>
+        <span style={styles.infos}>
+          {this.props.grade}
+          <br />
+        </span>
+        <span style={styles.infos}>{this.props.post}</span>
+      </div>
+    );
+  }
 }
 
 export default TeamItem;
