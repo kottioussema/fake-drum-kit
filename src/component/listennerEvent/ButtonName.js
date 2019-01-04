1921c1f25e9b6a75e6f35ab37c0e300cc52a71aa
Oussema <oussema@MacBook-Pro-de-macbook.local>
2019-01-04 12:46:28 +0100

    setting state

--- src/component/listennerEvent/ButtonName.js
+++ src/component/listennerEvent/ButtonName.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 
 class Name extends Component {
+
+  handleClick = () => {
+    this.props.onClick(this.props.name)
+  }
   render() {
     return (
       <div>
-        <button className="button" onClick={this.props.onClick} >
+        <button className="button" onClick={this.handleClick} >
           <span className="buttonText">
             {this.props.name}
           </span>
--- src/container/CheckpointReact6.js
+++ src/container/CheckpointReact6.js
@@ -5,28 +5,28 @@ const BUTTONS = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
 
 class CheckpointReact6 extends Component {
 	state = {
+		index:0,
 		number: 0
 	};
 
-	onClick = () => {
-		const prop = BUTTONS.map((item,index) => item * item);
-		const test= BUTTONS.forEach(function(item, index) {
-			console.log("test", test);
-			console.log("index",item, index);
-		});
-		console.log(prop[5])
+	onClick = (name) => {
+		let fact = 1;
+		for(let i=name; i>0; i--){
+			fact = i* fact;
+		}
 		this.setState({
-			number: prop[5]
+			index: name,
+			number: fact
 		});
 	};
 
 	render() {
 		return (
-			<div className="">
+			<div>
 				<div className="row">
-					{BUTTONS.map((item) => <ButtonName name={item} onClick={() => this.onClick()} />)}
+					{BUTTONS.map((item) => <ButtonName name={item} onClick={this.onClick} />)}
 				</div>
-				<span>{this.state.number}</span>
+				<span>the factorial of {this.state.index} is : {this.state.number} </span>
 			</div>
 		);
 	}
